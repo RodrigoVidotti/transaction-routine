3b1c6ddbd21f748489188a90c1116a91db0925f8
Rodrigo Vidotti <rodrigo.vidotti4@gmail.com>
2023-07-19 19:54:21 -0300

    feat: 🦺  adding validations to create transaction

--- controller/createTransaction.go
+++ controller/createTransaction.go
@@ -17,12 +17,34 @@ func CreateTransaction(c echo.Context) error {
 
 	conn := db.GetConn()
 
+	errMsg := validateAmount(t.Amount, t.OperationTypeId)
+	if errMsg != nil {
+		return c.JSON(http.StatusBadRequest, errMsg)
+	}
+
 	transactionID, err := InsertTransaction(conn, t)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	return c.JSON(http.StatusCreated, transactionID)
+}
+
+func validateAmount(amount float64, operationTypeID int) error {
+	if amount == 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Amount cannot be zero")
+	}
+
+	validSignal, ok := model.ValidOperationTypes[operationTypeID]
+	if !ok {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid operation type")
+	}
+
+	if amount*float64(validSignal) < 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Amount signal is not compatible with the operation type")
+	}
+
+	return nil
 }
 
 func InsertTransaction(conn *sql.DB, t *model.Transaction) (int64, error) {
--- model/operationType.go
+++ model/operationType.go
@@ -1,6 +1,18 @@
 package model
 
 type OperationType struct {
-	OperationTypeID string `json:"operation_type_id"`
+	OperationTypeID int    `json:"operation_type_id"`
 	Name            string `json:"name"`
+}
+
+const (
+	Debit  = -1
+	Credit = 1
+)
+
+var ValidOperationTypes = map[int]int{
+	1: Debit,  // COMPRA A VISTA
+	2: Debit,  // COMPRA PARCELADA
+	3: Debit,  // SAQUE
+	4: Credit, // PAGAMENTO
 }
